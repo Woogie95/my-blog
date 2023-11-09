1c68793d2745a458bee61eb8b098be19334e9514
Woogie95 <choisw950928@gmail.com>
2023-11-09 23:54:07 +0900

    feat: member 엔티티 추가

--- src/main/java/com/example/myblog/entity/Entity.java
+++
@@ -1,4 +0,0 @@
-package com.example.myblog.entity;
-
-public class Entity {
-}
---
+++ src/main/java/com/example/myblog/entity/Member.java
@@ -0,0 +1,35 @@
+package com.example.myblog.entity;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 20)
+    private String username;
+
+    @Column(nullable = false, length = 100)
+    private String password;
+
+    @Column(nullable = false, length = 50)
+    private String email;
+
+    private String role;
+
+    @CreationTimestamp
+    private Timestamp createdDate;
+
+}
