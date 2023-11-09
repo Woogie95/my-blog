7385e469d7caf3d7a774c3479bf24fc05d2e7de7
Woogie95 <choisw950928@gmail.com>
2023-11-10 00:14:55 +0900

    feat: Post 엔티티 추가

---
+++ src/main/java/com/example/myblog/entity/Post.java
@@ -0,0 +1,38 @@
+package com.example.myblog.entity;
+
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Lob
+    private String content;
+
+    @ColumnDefault("0")
+    private long viewCount;
+
+    @ManyToOne
+    @JoinColumn(name = "memberId")
+    private Member member;
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+}
